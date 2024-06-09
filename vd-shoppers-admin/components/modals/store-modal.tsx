16af4eb60abef93e4840e8450ae8308ae7c6de31
VyomOp2 <vyom.malo6904@gmail.com>
2024-06-09 19:28:27 +0530

    Updation

--- vd-shoppers-admin/components/modals/store-modal.tsx
+++ vd-shoppers-admin/components/modals/store-modal.tsx
@@ -1,10 +1,37 @@
 "use client";
 
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+
+// Defining the Form Schema and setting up Constrains with the help od using ZOD
+const formSchema = z.object({
+	name: z.string().min(1),
+});
+
 
 export const StoreModal = () => {
 	const storeModal = useStoreModal();
+
+	const form = useForm<z.infer<typeof formSchema>> ({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	}
+
 	return (
 		<Modal
 			title="Create Store"
@@ -12,7 +39,33 @@ export const StoreModal = () => {
 			isOpen={storeModal.isOpen}
 			onClose={storeModal.onClose}
 		>
-			Future Create Store Form
+			<div className="space-y-4 py-2 pb-4">
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							control={form.control}
+							name="name"
+							render={ ({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input placeholder="VD-Shoppers" {...field}/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className="pt-6 space-x-2 flex items-center justify-end w-full">
+							<Button 
+								variant="outline"
+								onClick={storeModal.onClose}>
+								Cancel
+							</Button>
+							<Button type="submit">Continue</Button>
+						</div>
+					</form>
+				</Form>
+			</div>
 		</Modal>
 	);
 };
