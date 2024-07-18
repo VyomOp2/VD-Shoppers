a44373a9b811e376a8689dcec4c43e47ac2a8bcd
VyomOp2 <vyom.malo6904@gmail.com>
2024-07-18 22:25:43 +0530

    Product card Preview Issue Resolved

--- vd-shoppers-frontend/components/ui/modal.tsx
+++ vd-shoppers-frontend/components/ui/modal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Dialog, Transition, TransitionChild } from "@headlessui/react";
 import { Fragment } from "react";
@@ -6,59 +6,46 @@ import IconButton from "./icon-button";
 import { X } from "lucide-react";
 
 interface ModalProps {
-    open : boolean;
-    onClose : () => void;
-    children : React.ReactNode;
+	open: boolean;
+	onClose: () => void;
+	children: React.ReactNode;
 }
 
-const Modal : React.FC<ModalProps> = ({
-    open,
-    onClose,
-    children
-}) => {
-    return ( 
-        
-        <Transition show={open} appear as={Fragment}>
-        
-            <Dialog as="div" className="relative z-10" onClose={onClose}>
-        
-                <div className="fixed inset-0 bg-black bg-opacity-50"/>
-        
-                <div className="fixed inset-0 overflow-y-auto">
-        
-                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+const Modal: React.FC<ModalProps> = ({ open, onClose, children }) => {
+	return (
+		<Transition show={open} appear as={Fragment}>
+			<Dialog as="div" className="relative z-10" onClose={onClose}>
+				<div className="fixed inset-0 bg-black bg-opacity-50" />
 
-                        <TransitionChild
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        />
-                            <Dialog.Panel className="w-full max-w-3xl overflow-hidden rounded-lg text-left align-middle">
+				<div className="fixed inset-0 overflow-y-auto">
+					<div className="flex min-h-full items-center justify-center p-4 text-center">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 scale-95"
+							enterTo="opacity-100 scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 scale-100"
+							leaveTo="opacity-0 scale-95"
+						>
+							<Dialog.Panel className="w-full max-w-3xl overflow-hidden rounded-lg text-left align-middle">
+								<div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+									<div className="absolute right-4 top-4">
+										<IconButton
+											onClick={onClose}
+											icon={<X size={25} />}
+										/>
+									</div>
 
-                                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+									{children}
+								</div>
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition>
+	);
+};
 
-                                    <div className="absolute right-4 top-4">
-                                        <IconButton onClick={onClose} icon={<X size={25} />} />
-                                    </div>
-                                    
-                                    {children}
-                                
-                                </div>
-                            
-                            </Dialog.Panel>
-                    
-                    </div>
-
-                </div>
-            
-            </Dialog>
-
-        </Transition>
-    );
-}
- 
 export default Modal;
